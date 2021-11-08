67b9321b26210dcf27ef5e314174913e1155c836
xingyijun <xing_yijun@uxsino.com>
2021-11-08 18:07:55 +0800

    新增登录接口,优化文件读取

--- app.js
+++ app.js
@@ -4,45 +4,42 @@ const fs = require('fs')
 const formidable = require('express-formidable'); //解析formdata
 const cors = require('cors');
 const WebSocket = require("ws");
-let userData = []
-let wss = new WebSocket.Server({ port: 3010 });
 
+let wss = new WebSocket.Server({ port: 3010 });
+let userData = []
 
 
 
 app.use((req, res, next) => {
   //读取文件
+  console.log(req, 5566633)
   fs.readFile('user.json', (error, data) => {
-    data = JSON.parse(data)
+    console.log(JSON.parse(data))
+    userData = JSON.parse(data)
+    next()
   })
-  // 广播
-  // wss.broadcast = function(msg) {
-  //   wss.clients.forEach(function each (client) {
-  //     client.send(msg.toString());
-  //   });
-  // };
-  next()
 })
 
 // 连接时触发
 // 参考https://blog.csdn.net/qq_41097495/article/details/105835100
 wss.on("connection", function(ws) {//先连接
   ws.on("message", function(data) {//用message来监听客户端发来的消息
-    console.log(JSON.parse(data))
+    console.log(JSON.parse(data), 555666)
     let info = JSON.parse(data);
     if (info.type === 'login') {
       ws['user'] = info.id;
-      fs.writeFile('user.json', (error, data) => {
-        console.log(data)
-      })
     } else {
       // 如果是信息请求 则遍历wss.clients这个客户端set对象
       // 注意 这个对象是set类型 所以需要使用forEach进行遍历
       wss.clients.forEach(item => {
+        console.log(item['user'], info.toId, 'item.user')
         // 如果遍历到的客户端的user和info中的to相同 则发送信息给该客户端
         if (item['user'] === info.toId) {
           console.log(122)
-          item.send(info)
+          item.send(JSON.stringify(info))
+        } else {
+          info.failSend = true
+          item.send(JSON.stringify(info))
         }
       });
     }
@@ -64,33 +61,37 @@ wss.on("close", function(data) {//用message来监听客户端发来的消息
 const port = 3000;
 app.use(cors());
 app.use(formidable());
-
+app.post('/login', (req, res, next) => {
+  const { name, password } = req.fields
+  let userArr = userData.filter(item => {
+    return item.id === name
+  })
+  if (userArr.length > 0) {
+    if (userArr[0].password === password) {
+      res.json({
+        code: 200, data: userArr[0], message: '666666'
+      })
+    } else {
+      res.json({
+        code: 401, data: [], message: '密码不正确'
+      })
+    }
+  } else {
+    res.json({
+      code: 401, data: [], message: '该账户尚未注册'
+    })
+  }
+})
 app.get('/chatList', (req, res, next) => {
-  console.log(req.query)
+  // let userData = []
+  // fs.readFile('user.json', (error, data) => {
+  // console.log(JSON.parse(data))
+  // userData = JSON.parse(data)
   res.status('200').send({
     code: 200, message: '666666',
-    data: [{
-      icon: '',
-      name: 'mychat',
-      id: 'fafdsafasd'
-    }, {
-      icon: '',
-      name: 'mychat1',
-      id: 'sgaasdgg'
-    }, {
-      icon: '',
-      name: 'mychat2',
-      id: 'bvcbxcvzvz'
-    }, {
-      icon: '',
-      name: 'mychat3',
-      id: 'erhrtggd'
-    }, {
-      icon: '',
-      name: 'mychat4',
-      id: 'jhjhfgcn'
-    }]
+    data: userData
   })
+  // })
 })
 
 
