328e531e59912096abc33644bb458bd457948111
xingyijun <xing_yijun@uxsino.com>
2021-11-09 18:14:57 +0800

    修改点对点逻辑

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ let userData = []
 
 app.use((req, res, next) => {
   //读取文件
-  console.log(req, 5566633)
   fs.readFile('user.json', (error, data) => {
     console.log(JSON.parse(data))
     userData = JSON.parse(data)
@@ -24,24 +23,46 @@ app.use((req, res, next) => {
 // 参考https://blog.csdn.net/qq_41097495/article/details/105835100
 wss.on("connection", function(ws) {//先连接
   ws.on("message", function(data) {//用message来监听客户端发来的消息
-    console.log(JSON.parse(data), 555666)
+    // console.log(JSON.parse(data), 555666)
     let info = JSON.parse(data);
     if (info.type === 'login') {
       ws['user'] = info.id;
     } else {
+      let clientArr = []
+      let isOnline = null
+      let userClient = null
       // 如果是信息请求 则遍历wss.clients这个客户端set对象
       // 注意 这个对象是set类型 所以需要使用forEach进行遍历
       wss.clients.forEach(item => {
         console.log(item['user'], info.toId, 'item.user')
+        clientArr.push(item)
         // 如果遍历到的客户端的user和info中的to相同 则发送信息给该客户端
         if (item['user'] === info.toId) {
-          console.log(122)
-          item.send(JSON.stringify(info))
-        } else {
-          info.failSend = true
-          item.send(JSON.stringify(info))
+          isOnline = item
+        } else if (item['user'] === info.uuid) {
+          userClient = item
         }
+        //  else {
+        //   info.failSend = true
+        //   item.send(JSON.stringify(info))
+        // }
       });
+      // let currentClient = null
+      // let isOnline = clientArr.filter(item => {
+      //   return item['user'] === info.toId
+      // })
+      console.log(isOnline, 999)
+      if (isOnline) {
+        // if (item['user'] === info.toId) {
+        // console.log(122)
+        info.failSend = false
+        isOnline.send(JSON.stringify(info))
+        userClient.send(JSON.stringify(info))
+        // }
+      } else {
+        info.failSend = true
+        userClient.send(JSON.stringify(info))
+      }
     }
     // 调用广播
     // wss.broadcast(data)
